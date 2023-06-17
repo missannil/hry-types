bcf8cd47aed24f3e8c6f027a5bdfecb4a2358162
missannil <missannil@163.com>
2023-06-17 21:46:22 +0800

    fix(Object):  ReturnTypeOfProperty rename  ReturnType

---
+++ src/Object/ReturnType.ts
@@ -0,0 +1,15 @@
+import { type AnyFunction, type AnyObject } from "..";
+
+/**
+ * 把对象子属性类型为函数类型的,变为函数返回类型
+ * @example
+ * ```ts
+ * import { TypeChecking, type O,type Test} from 'hry-types'
+ *
+ * TypeChecking<O.ReturnType<{ fun: () => number; num: number }>, { fun: number; num: number }, Test.Pass>;
+ * ```
+ * @returns object
+ */
+export type ReturnType<O extends AnyObject> = {
+  [k in keyof O]: O[k] extends AnyFunction ? ReturnType<O[k]> : O[k];
+};
--- src/Object/_api.ts
+++ src/Object/_api.ts
@@ -3,6 +3,6 @@ export type { FilterKeys } from "./FilterKeys";
 export type { IsPureObject } from "./IsPureObject";
 export type { MergeIntersection } from "./MergeIntersection";
 export type { NonReadonly } from "./NonReadonly";
-export type { ReturnTypeOfProperty } from "./ReturnTypeOfProperty";
+export type { ReturnType } from "./ReturnType";
 export type { Select } from "./Select";
 export type { SelectKeys } from "./SelectKeys";
