cb2277e12b6bb9437a998f5002e207ca141659be
missannil <missannil@163.com>
2023-06-22 16:34:41 +0800

    fix: 导出Asss类型

--- src/Any/_api.ts
+++ src/Any/_api.ts
@@ -1,8 +1,9 @@
-export type { Cast } from "./Cast";
-export type { Contains } from "./Contains";
-export type { Equals } from "./Equals";
-export type { Extends } from "./Extends";
-export type { IfEquals } from "./IfEquals";
-export type { IfExtends } from "./IfExtends";
-export type { Is } from "./Is";
-export type { IsNever } from "./IsNever";
+export type { As } from './As';
+export type { Cast } from './Cast';
+export type { Contains } from './Contains';
+export type { Equals } from './Equals';
+export type { Extends } from './Extends';
+export type { IfEquals } from './IfEquals';
+export type { IfExtends } from './IfExtends';
+export type { Is } from './Is';
+export type { IsNever } from './IsNever';
