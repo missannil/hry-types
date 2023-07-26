ce5a4895f8e7cbf68c38ac1082aa709a23ecc219
missannil <missannil@163.com>
2023-07-26 15:12:08 +0800

    chore: illegalFieldValidator

--- test/Any/Cast.test.ts
+++ test/Any/Cast.test.ts
@@ -1,22 +1,22 @@
 // 测试 path: ./src/Any/Cast.test.ts
 
-// import { type Test, TypeChecking } from "../../src";
+// import { type Test, Checking } from "../../src";
 // import type { Cast } from "../../src/Any/_Cast";
 
 // type Test1 = Cast<0, number>;
 
 // type Test1Expect = 0;
 
-// TypeChecking<Test1, Test1Expect, Test.Pass>;
+// Checking<Test1, Test1Expect, Test.Pass>;
 
 // type Test2 = Cast<0 | 1, number>;
 
 // type Test2Expect = 0 | 1;
 
-// TypeChecking<Test2, Test2Expect, Test.Pass>;
+// Checking<Test2, Test2Expect, Test.Pass>;
 
 // type Test3 = Cast<0 | "str", number>;
 
 // type Test3Expect = number;
 
-// TypeChecking<Test3, Test3Expect, Test.Pass>;
+// Checking<Test3, Test3Expect, Test.Pass>;
