9feb919bc799f4ee4260b6dc9d43ae3c14282c95
missannil <missannil@163.com>
2023-07-28 13:00:01 +0800

    chore: "omputeUnion" 去除ComputeIntersection包裹
    Release-As: 0.16.2

--- src/Object/UnionOf.ts
+++ src/Object/UnionOf.ts
@@ -1,12 +1,10 @@
-import type { ComputeIntersection } from "./ComputeIntersection";
-
 /**
  * 合并两个对象,相同key类型联合,其余key不变
  * @example
  * ```ts
  * type O1 = { a: string; b: number; c: boolean };
  * type O2 = { a: number; b: string; d: string };
- * type Test = UnionOfTwoObject<O1, O2>;
+ * type Test = UnionOf<O1, O2>;
  * // =>{ a: string | number; b: number | string; c: boolean; d: string }
  * ```
  */
@@ -14,8 +12,7 @@ export type UnionOf<
   O1,
   O2,
   SameKey extends Extract<keyof O1, keyof O2> = Extract<keyof O1, keyof O2>,
-> = ComputeIntersection<
+> =
   & { [k in SameKey]: O1[k] | O2[k] }
   & Omit<O1, SameKey>
-  & Omit<O2, SameKey>
->;
+  & Omit<O2, SameKey>;
