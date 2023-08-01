cd1ea18d09c2da569e594b79f80710f5bfa631e2
missanni <missannil@163.com>
2023-08-01 13:02:12 +0800

    chore(Checking): 修改注释

--- src/Check/Checking.ts
+++ src/Check/Checking.ts
@@ -4,15 +4,15 @@ import type { Equals } from "../Any/Equals";
  * 类型相等验证,用于测试。这是一个函数,但无需调用它
  * @example
  * ```ts
- * EqualsChecking<1, 1, Test.Pass>;
- * EqualsChecking<1, 0, Test.Fail>;
+ * Checking<1, 1, Test.Pass>;
+ * Checking<1, 0, Test.Fail>;
  * ```
  */
 export function Checking<
   A,
   B,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  _Expect extends Equals<A, B>,
+  _Result extends Equals<A, B>,
 > // eslint-disable-next-line @typescript-eslint/no-empty-function
 () {
 }
